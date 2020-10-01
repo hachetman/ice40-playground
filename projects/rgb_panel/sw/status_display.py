2d9a5ff11205f014b67736dc4bf058b6735bb18d
Christian Grumbein <christian@grumbein.de>
2020-10-01 18:44:42 +0200

    WIP: working on status display

--- projects/rgb_panel/sw/status_display.py
+++ projects/rgb_panel/sw/status_display.py
@@ -1,85 +1,70 @@
 #!/usr/bin/env python3
 
-import argparse
 import time
-import numpy as np
-from PIL import Image, ImageDraw, ImageFont, BdfFontFile
-from datetime import datetime
 
 import control
+import updater
 
 
 class PanelControl(control.BoardControlBase):
 
-	def __init__(self, n_banks=2, n_rows=16, n_cols=64, colordepth=16, **kwargs):
-		# Super call
-		super().__init__(**kwargs)
+    def __init__(self, n_banks=2, n_rows=16, n_cols=64, colordepth=16,  **kwargs):
+        # Super call
+        super().__init__(**kwargs)
 
-		# Save panel description
-		self.n_banks = n_banks
-		self.n_rows  = n_rows
-		self.n_cols  = n_cols
-		self.colordepth = colordepth
+        # Save panel description
+        self.n_banks = n_banks
+        self.n_rows  = n_rows
+        self.n_cols  = n_cols
+        self.colordepth = colordepth
 
-		# Pre-create buffers
-		self.line_bytes = n_cols * (colordepth // 8)
-		self.send_buf = bytearray(1 + self.line_bytes)
-		self.send_buf_view = memoryview(self.send_buf)
+        # Pre-create buffers
+        self.line_bytes = n_cols * (colordepth // 8)
+        self.send_buf = bytearray(1 + self.line_bytes)
+        self.send_buf_view = memoryview(self.send_buf)
 
-	def send_line_data(self, data):
-		self.send_buf_view[0] = 0x80
-		self.send_buf_view[1:] = data
-		self.slave.exchange(self.send_buf)
+    def send_line_data(self, data):
+        self.send_buf_view[0] = 0x80
+        self.send_buf_view[1:] = data
+        self.slave.exchange(self.send_buf)
+
+    def send_frame_data(self, frame):
+        # View on the data
+        frame_view = memoryview(frame)
+        # Scan all line
+        for y in range(self.n_banks * self.n_rows):
+            # Send write command to line buffer
+            self.send_line_data(frame_view[y*self.line_bytes:(y+1)*self.line_bytes])
+            # Swap line buffer & Write it to line y of back frame buffer
+            self.reg_w8(0x03, y)
+
+        # Send frame swap command
+        self.reg_w8(0x04, 0x00)
+
+        # Wait for the frame swap to occur
+        while (self.read_status() & 0x02 == 0):
+            pass
+
+    def assemble_buffer(self, image):
+        frame_buffer = bytearray(64*32*2)
+        for x in range(image.width):
+            for y in range(image.height):
+                pixel = image.getpixel((x,y))
+                # red and green values
+                frame_buffer[y * 128 + x*2 + 1]= pixel[0] & 0xf8 | pixel[1] >> 5
+                # blue values
+                frame_buffer[y * 128 + x*2] = (pixel[1] << 5) & 0xe0 | pixel[2] >> 3
+        return frame_buffer
 
 
-	def send_frame_data(self, frame):
-		# View on the data
-		frame_view = memoryview(frame)
-		# Scan all line
-		for y in range(self.n_banks * self.n_rows):
-			# Send write command to line buffer
-			self.send_line_data(frame_view[y*self.line_bytes:(y+1)*self.line_bytes])
-			# Swap line buffer & Write it to line y of back frame buffer
-			self.reg_w8(0x03, y)
-
-		# Send frame swap command
-		self.reg_w8(0x04, 0x00)
-
-		# Wait for the frame swap to occur
-		while (self.read_status() & 0x02 == 0):
-			pass
-
-	def assemble_buffer(self, image):
-		frame_buffer = bytearray(64*32*2)
-		for x in range(image.width):
-			for y in range(image.height):
-				pixel = image.getpixel((x,y))
-				# red and green values
-				frame_buffer[y * 128 + x*2 + 1]= pixel[0] & 0xf8 | pixel[1] >> 5
-				# blue values
-				frame_buffer[y * 128 + x*2] = (pixel[1] << 5) & 0xe0 | pixel[2] >> 3
-		return frame_buffer
 def main():
-	panel = PanelControl()
-
-
-	#frame_buffer = bytearray(64*32*2)
-	im = Image.new('RGBA', (64, 32), (0,0,0,0))
-	#fnt = ImageFont.truetype("examples/pixelmix.ttf", 8)
-	#fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40)
-	fnt = ImageFont.load("./fonts/7x13.pil")
-
-	draw = ImageDraw.Draw(im)
-	#draw.line((0, 0, 20, 16), fill=6556)
-	while(True):
-		now = datetime.now()
-		draw.rectangle((0,0,64,32), fill=(0,0,0,0))
-		draw.text((3,-1), now.strftime("%H:%M-%S"), font=fnt, fill=(32,32,32,0))
-		#panel.send_frame_data(im.tobytes())
-		frame_buffer = panel.assemble_buffer(im)
-		panel.send_frame_data(frame_buffer)
-		time.sleep(1)
-
+    panel = PanelControl()
+    update = updater.updater()
+    update.start()
+    while(True):
+        frame_buffer = panel.assemble_buffer(update.get_draw_buffer())
+        panel.send_frame_data(frame_buffer)
+        time.sleep(1)
 
 if __name__ == '__main__':
-	main()
+    main()
--- projects/rgb_panel/sw/updater.py
+++ projects/rgb_panel/sw/updater.py
@@ -2,17 +2,121 @@
 
 from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
+import threading
+import time
+import requests
+import json
 
 
-class updater():
+class updater(threading.Thread):
     def __init__(self):
+        threading.Thread.__init__(self)
         self.im = Image.new('RGBA', (64, 32), (0,0,0,0))
-        self.fnt = ImageFont.load("./fonts/7x13.pil")
+        self.time_font = ImageFont.load("./fonts/9x15.pil")
+        self.status_font = ImageFont.load("./fonts/6x9.pil")
+        self.time_color = (255, 255, 255, 0)
+        self.status_color = (255, 255, 255, 0)
         self.draw = ImageDraw.Draw(self.im)
+        self.timer = time.time()
+        self.base_url="https://dynamic.grumbein.de/api/states/"
+        self.headers = {
+                'Authorization': 'Bearer <enter bearer>',
+                'content-type': 'application/json',
+            }
+        self.temperature = 0
+        self.co2 = 0
+        self.humidity = 0
+        self.rain = 0
+        self.timer = time.time()
+        self.ping = "100"
+        self.sun = "above_horizon"
+        self.seconds = 0
 
-    def update(self):
+    def get_draw_buffer(self):
+        if self.sun == "above_horizon":
+            self.time_color = (255, 255, 255, 0)
+            self.status_color = (255, 255, 255, 0)
+        else:
+            self.time_color = (32, 32, 32, 0)
+            self.status_color = (32, 32, 32, 0)
+
         now = datetime.now()
         self.draw.rectangle((0, 0, 64, 32), fill=(0, 0, 0, 0))
-        self.draw.text((3, -1), now.strftime("%H %M %S"),
-                       font=self.fnt, fill=(32, 32, 32, 0))
+        self.draw.text((0, -2), now.strftime("%H"),
+                       font=self.time_font,
+                       fill=self.time_color)
+        self.draw.text((24, -2), now.strftime("%M"),
+                       font=self.time_font,
+                       fill=self.time_color)
+        self.draw.text((46, -2), now.strftime("%S"),
+                       font=self.time_font,
+                       fill=self.time_color)
+        self.draw.text((0, 10), "T:" + str(int(self.temperature)),
+                       font=self.status_font,
+                       fill=self.status_color)
+        self.draw.text((23, 9), "°",
+                       font=self.status_font,
+                       fill=self.status_color)
+        self.draw.text((0, 17), "P:" + str(int(self.ping)),
+                       font=self.status_font,
+                       fill=self.status_color)
+        self.draw.text((35, 10), "H:" + str(int(self.humidity)) + "%",
+                       font=self.status_font,
+                       fill=self.status_color)
+        self.draw.text((35, 17), "R:" + str(int(self.rain)),
+                       font=self.status_font,
+                       fill=self.status_color)
+        self.draw.text((7, 25), "CO2:" + str(int(self.co2)),
+                       font=self.status_font,
+                       fill=self.status_color)
         return self.im
+
+    def request_wrapper(self, url, headers):
+        try:
+            response = requests.get(url, headers=headers)
+        except Exception as e:
+            print(time.strftime("%d:%m %H:%M") + " Request exception Homeassistant")
+            print(e)
+        return response
+
+    def update(self):
+        print("updating")
+        print(time.strftime("%d:%m %H:%M") + " Updating")
+        response = self.request_wrapper(self.base_url + "sensor.co2",
+                                        self.headers)
+        response_json = json.loads(response.text)
+        self.co2 = response_json['state']
+
+        response = self.request_wrapper(self.base_url + "sensor.openweathermap_humidity",
+                                         self.headers)
+        response_json = json.loads(response.text)
+        self.humidity = response_json['state']
+
+        response = self.request_wrapper(self.base_url + "sensor.openweathermap_temperature",
+                                         self.headers)
+        response_json = json.loads(response.text)
+        self.temperature = float(response_json['state'])
+
+        response = self.request_wrapper(self.base_url + "sensor.openweathermap_rain",
+                                         self.headers)
+        response_json = json.loads(response.text)
+        self.rain = response_json['state']
+        if self.rain == "not raining":
+            self.rain = 0.0
+
+        response = self.request_wrapper(self.base_url + "sun.sun",
+                                         self.headers)
+        response_json = json.loads(response.text)
+        self.sun = response_json['state']
+
+        response = self.request_wrapper(self.base_url + "binary_sensor.ping_heise_de", self.headers)
+        response_json = json.loads(response.text)
+        self.ping = float(response_json['attributes']['round_trip_time_avg'])
+
+    def run(self):
+        self.update()
+        while True:
+            time.sleep(10)
+            if (time.time() - self.timer > 60):
+                self.update()
+                self.timer = time.time()
